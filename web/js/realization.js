c9d149dd5b56918d519fc8f0bae1228373048897
Mr-Pike <marc.leaument@gmail.com>
2014-10-02 23:48:51 +0200

    - Mise à jour des réalisations - Correction du css

--- web/js/realization.js
+++ web/js/realization.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-    $('.project').hover(function() {
-        /*$(this).find('img').fadeTo(500, 0.2);*/
-        var that = $(this);
-        $(this).find('img').fadeTo(500, 0.2, function() {
-            that.find('.title').fadeIn();
-            that.find('.info').fadeIn();
-            that.find('.more').fadeIn();
-        });
-    }, function() {
-        var that = $(this);
-        $(this).find('img').fadeTo(500, 1, function() {
-            that.find('.title').fadeOut();
-            that.find('.info').fadeOut();
-            that.find('.more').fadeOut();
-        });
-    });
+    $('.project').hover(
+        function() {
+            var that = $(this);
+            $(this).find('img').fadeTo(500, 0.2, function() {
+                if(that.is(':hover')) {
+                    that.find('.title, .info, .more').fadeIn();
+                    console.log('yo');
+                }
+            });
+        }, function() {
+            var that = $(this);
+            $(this).find('img').fadeTo(500, 1, function() {
+                that.find('.title, .info, .more').fadeOut();
+            });
+        }
+    );
 });
